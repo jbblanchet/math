63223c575fe7b44d94f1a9375e6b56499e33d040
jbblanchet <jbblanchet@gmail.com>
2020-01-11 17:36:36 -0500

    Additions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function rand () {
+  return Math.round(Math.random() * 9)
 }
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    num1: rand(),
+    num2: rand(),
+    response: ""
+  }
+
+  onChange = (e) => {
+    this.setState({ response: e.target.value })
+  }
+
+  onKeyDown = (e) => {
+    if (e.key === "Enter") {
+      if (parseInt(e.target.value) === this.state.num1 + this.state.num2) {
+        this.setState({
+          num1: rand(),
+          num2: rand(),
+          response: ""
+        })
+      }
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>{this.state.num1} + {this.state.num2} = <input value={this.state.response} onChange={this.onChange} onKeyDown={this.onKeyDown} type="number"></input></div>
+        </header>
+      </div>
+    );
+  }
+}
