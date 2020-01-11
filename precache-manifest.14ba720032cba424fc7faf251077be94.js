b0ce8be530b06f36b70f637ffe1186722500b4e1
jbblanchet <jbblanchet@gmail.com>
2020-01-11 18:05:29 -0500

    Updates

---
+++ precache-manifest.14ba720032cba424fc7faf251077be94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9964bcc325e1ebcbdf3c0507f49d15f",
+    "url": "/math/index.html"
+  },
+  {
+    "revision": "f13717bf81d1c944e593",
+    "url": "/math/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a0f8fcd096b6421e244",
+    "url": "/math/static/js/2.bc11f040.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/math/static/js/2.bc11f040.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f13717bf81d1c944e593",
+    "url": "/math/static/js/main.bac3249c.chunk.js"
+  },
+  {
+    "revision": "e22afdc838650c8f1f6d",
+    "url": "/math/static/js/runtime-main.10cde7d7.js"
+  }
+]);
